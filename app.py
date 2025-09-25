ea4049c6d8a054b4432bb0740159995bec8b2e11
dirkfischer <fischertechnic@gmx.net>
2025-09-25 19:13:56 +0200

    added json, index, style.css and updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+# Datei mit den Blogposts
+DATA_FILE = "posts.json"
+
+# Hilfsfunktion: JSON-Datei laden
+def load_posts():
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+# Hilfsfunktion: JSON-Datei speichern
+def save_posts(posts):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(posts, f, indent=4, ensure_ascii=False)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    posts = load_posts()  # Beiträge laden
+    return render_template("index.html", posts=posts)
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
